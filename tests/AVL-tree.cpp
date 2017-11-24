3b01eecfff8fa5c042e848595d77e2159b9a22f7
kate-lozovaya <katekalozovaya@gmail.com>
2017-11-24 19:45:08 +0300

    Update AVL-tree.cpp

--- tests/AVL-tree.cpp
+++ tests/AVL-tree.cpp
@@ -2,10 +2,17 @@
 
 #include "AVL-tree.hpp"
 
-TEST_CASE("insert_root", "[root]") {
-   SplayTree<int> testSplayTree;
-   testSplayTree.insert(10);
-   REQUIRE(testSplayTree.search(10) == 1);
+TEST_CASE("insert", "[root]") 
+{
+   AVL_tree<int> tree;
+   tree.insert(10);
+   Node<int> * node = search(10);
+   REQUIRE(tree.left_(node) == nullptr);
+   REQUIRE(tree.right_(node) == nullptr);
+   REQUIRE(trre.parent_(node) == nullptr);
+   REQUIRE(tree.key_(node) == 10);
+   REQUIRE(tree.height_(node) == 1);
+   REQUIRE(tree.count_() == 1);
 }
 
 
