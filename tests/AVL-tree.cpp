bb6c524e655f628cd8e743055cdf71751fdf4c7e
kate-lozovaya <katekalozovaya@gmail.com>
2017-11-24 20:07:21 +0300

    Update AVL-tree.cpp

--- tests/AVL-tree.cpp
+++ tests/AVL-tree.cpp
@@ -6,10 +6,10 @@ TEST_CASE("insert", "[root]")
 {
    AVL_tree<int> tree;
    tree.insert(10);
-   Node<int> * node = search(10);
+   Node<int> * node = tree.search(10);
    REQUIRE(tree.left_(node) == nullptr);
    REQUIRE(tree.right_(node) == nullptr);
-   REQUIRE(trre.parent_(node) == nullptr);
+   REQUIRE(tree.parent_(node) == nullptr);
    REQUIRE(tree.key_(node) == 10);
    REQUIRE(tree.height_(node) == 1);
    REQUIRE(tree.count_() == 1);
