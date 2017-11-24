980d2f85c338778405c329129d5fa17d3887cc27
kate-lozovaya <katekalozovaya@gmail.com>
2017-11-24 21:33:11 +0300

    Update AVL-tree.cpp

--- tests/AVL-tree.cpp
+++ tests/AVL-tree.cpp
@@ -39,3 +39,27 @@ TEST_CASE("insert", "[rotate_right]")
    REQUIRE(tree.height_(tree.search(10)) == 1);
    REQUIRE(tree.count_() == 3);
 }
+
+/*  8                        9
+      \                    /   \       
+        9           ->    8     10        
+          \
+           10                     */
+TEST_CASE("insert", "[rotate_left]") 
+{
+   AVL_tree<int> tree;
+   tree.insert(8);
+   tree.insert(9);
+   tree.insert(10);
+   Node<int> * node = tree.search(9);
+   REQUIRE(tree.left_(node) == tree.search(8));
+   REQUIRE(tree.right_(node) == tree.search(10));
+   REQUIRE(tree.parent_(node) == nullptr);
+   REQUIRE(tree.key_(node) == 9);
+   REQUIRE(tree.height_(node) == 2);
+   REQUIRE(tree.parent_(tree.search(8)) == node);
+   REQUIRE(tree.parent_(tree.search(10)) == node);
+   REQUIRE(tree.height_(tree.search(8)) == 1);
+   REQUIRE(tree.height_(tree.search(10)) == 1);
+   REQUIRE(tree.count_() == 3);
+}
