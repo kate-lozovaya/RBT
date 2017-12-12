9b5bb3c6c84172bd4881147ed5122bc97a14ec30
kate-lozovaya <katekalozovaya@gmail.com>
2017-12-12 20:42:44 +0300

    Update RBT.hpp

--- include/RBT.hpp
+++ include/RBT.hpp
@@ -3,413 +3,421 @@
 #include <stdexcept>
 using namespace std;
 
-enum Color{ RED, BLACK };
+enum Color{ RED, BLACK, BLACK_BLACK };
 
 template<typename T>
 struct Node
 {
-	Node<T> * left;
-	Node<T> * right;
-	Node<T> * parent;
-	Color color;
-	T value;
-	Node(T const & value) : value{ value }, left{ nullptr }, right{ nullptr }, parent{ nullptr }, color{ RED } {}
+	Node<T> * left_;
+	Node<T> * right_;
+	Node<T> * parent_;
+	Color color_;
+	T key_;
+	Node(T const & key) : key_{ key }, left_{ nullptr }, right_{ nullptr }, parent_{ nullptr }, color_{ RED } {}
 };
 
 template<typename T>
 class Tree
 {
 private:
-	Node<T> * root;
-	unsigned int count;
+	Node<T> * root_;
+	unsigned count_;
 
-	void deleteNode(Node<T> * node);
+	void deleteNode_(Node<T> * node);
 
-	ostream& print(ostream&stream);
-	ostream& print(Node<T> * node, ostream&stream, size_t level)const;
+	ostream& print_(ostream&stream);
+	ostream& print_(Node<T> * node, ostream&stream, size_t level)const;
 
-	void insert(Node<T> * node, const T& value);
-	void insertBalance(Node<T> * cur);
+	void insert_(Node<T> * node, const T& key);
+	void insertBalance_(Node<T> * cur);
 
-	Node<T>* deleteRoot(Node<T> * head);
-	void deleteBalance(Node<T> * node);
+	void changeColor_(Node<T> * node);
+	void rotateRight_(Node<T> * node);
+	void rotateLeft_(Node<T> * node);
+	Node<T> * sibling_(Node<T> * node);
 
-	void change_color(Node<T> * node);
-	void turn_right(Node<T> * node);
-	void turn_left(Node<T> * node);
-	Node<T> * sibling(Node<T> * node);
+	void deleteBalance_(Node<T> * node);
 public:
 	Tree();
 	~Tree();
 
-	Node<T> * search(const T& value)const;
-	Node<T> * left_(T key)
+	Node<T> * search(const T& key)const;
+	Node<T> * left(T key)
 	{
 		Node<T> * node = search(key);
-		return node->left;
+		return node->left_;
 	}
-	Node<T> * right_(T key)
+	Node<T> * right(T key)
 	{
 		Node<T> * node = search(key);
-		return node->right;
+		return node->right_;
 	}
-	Node<T> * parent_(T key)
+	Node<T> * parent(T key)
 	{
 		Node<T> * node = search(key);
-		return node->parent;
+		return node->parent_;
 	}
-	Color color_(T key)
+	Color color(T key)
 	{
 		Node<T> * node = search(key);
-		return node->color;
+		return node->color_;
 	}
-	Node<T> * root_()
+	Node<T> * root()
 	{
-		return root;
+		return root_;
 	}
-	unsigned int count_()
+	unsigned int count()
 	{
-		return count;
+		return count_;
 	}
 
 	friend ostream& operator << (ostream&stream, Tree<T> & tree)
 	{
-		return tree.print(cout);
+		return tree.print_(cout);
 	}
 
-	void insert(const T& value);
+	void insert(const T& key);
 
-	void deleteEl(const T& value);
+	void deleteElement(const T& key);
 };
 
 template<typename T>
-Tree<T>::Tree() : root{ nullptr }, count{ 0 } {};
+Tree<T>::Tree() : root_{ nullptr }, count_{ 0 } {};
 template<typename T>
 Tree<T>::~Tree()
 {
-	deleteNode(root);
+	deleteNode_(root_);
 }
 template<typename T>
-void Tree<T>::deleteNode(Node<T> * node)
+void Tree<T>::deleteNode_(Node<T> * node)
 {
 	if (node == nullptr) return;
-	deleteNode(node->left);
-	deleteNode(node->right);
+	deleteNode_(node->left_);
+	deleteNode_(node->right_);
 	delete node;
 }
 
 template<typename T>
-Node<T> * Tree<T>::search(const T& value)const
+Node<T> * Tree<T>::search(const T& key)const
 {
-	Node<T> * curEl = root;
-	while (curEl != nullptr)
+	Node<T> * cur = root_;
+	while (cur != nullptr)
 	{
-		if (curEl->value == value)
+		if (cur->key_ == key)
 			break;
 		else
 		{
-			if (value > curEl->value)
-				curEl = curEl->right;
-			else curEl = curEl->left;
+			if (key > cur->key_)
+				cur = cur->right_;
+			else cur = cur->left_;
 		}
 	}
-	return curEl;
+	return cur;
 }
 
 template<typename T>
-ostream & Tree<T>::print(ostream&stream)
+ostream & Tree<T>::print_(ostream&stream)
 {
-	print(root, stream, 0);
+	print_(root_, stream, 0);
 	return stream;
 }
 template<typename T>
-ostream & Tree<T>::print(Node<T> * node, ostream&stream, size_t level)const
+ostream & Tree<T>::print_(Node<T> * node, ostream&stream, size_t level)const
 {
 	Node<T> * cur = node;
 	if (cur != nullptr)
 	{
 		string a;
-		if (cur->color == RED)
+		if (cur->color_ == RED)
 			a = "r";
 		else a = "b";
-		print(cur->right, stream, level + 1);
+		print_(cur->right_, stream, level + 1);
 		for (unsigned int i = 0; i < level; ++i)
 			stream << '-';
-		stream << cur->value << " (" << a << ")" << endl;
-		print(cur->left, stream, level + 1);
+		stream << cur->key_ << " (" << a << ")" << endl;
+		print_(cur->left_, stream, level + 1);
 	}
 	return stream;
 }
 
 template<typename T>
-void Tree<T>::insert(const T& value)
+void Tree<T>::insert(const T& key)
 {
-	insert(root, value);
+	insert_(root_, key);
 }
 template<typename T>
-void Tree<T>::insert(Node<T> * node, const T& value)
+void Tree<T>::insert_(Node<T> * node, const T& key)
 {
 	Node<T> * parent = nullptr;
 	Node<T> * cur = node;
 	while (cur)
 	{
 		parent = cur;
-		if (value == cur->value)
-			throw logic_error("There is this element in the tree\n");
-		if (value < cur->value)
-			cur = cur->left;
-		else //if (value > node->value)
-			cur = cur->right;
+		/*if (key == cur->key_)
+			throw logic_error("There is this element in the tree\n");*/
+		if (key < cur->key_)
+			cur = cur->left_;
+		else //if (key > node->key_)
+			cur = cur->right_;
 	}
-	cur = new Node<T>(value);
-	count++;
-	cur->parent = parent;
-	if (cur->parent == nullptr)
+	cur = new Node<T>(key);
+	count_++;
+	cur->parent_ = parent;
+	if (cur->parent_ == nullptr)
 	{
-		root = cur;
-		root->color = BLACK;
+		root_ = cur;
+		root_->color_ = BLACK;
 	}
 	else
 	{
-		if (value < parent->value)
-			parent->left = cur;
-		else parent->right = cur;
-		insertBalance(cur);
+		if (key < parent->key_)
+			parent->left_ = cur;
+		else parent->right_ = cur;
+		insertBalance_(cur);
 	}
 }
 template<typename T>
-void Tree<T>::insertBalance(Node<T> * cur)
+void Tree<T>::insertBalance_(Node<T> * cur)
 {
-	while (cur != root && cur->parent->color == RED && cur->color == RED)
+	while (cur != root_ && cur->parent_->color_ == RED && cur->color_ == RED)
 	{
 		Node<T> * uncle = nullptr;
-		if (cur->parent->parent->left == cur->parent)
-			uncle = cur->parent->parent->right;
-		else uncle = cur->parent->parent->left;
+		if (cur->parent_->parent_->left_ == cur->parent_)
+			uncle = cur->parent_->parent_->right_;
+		else uncle = cur->parent_->parent_->left_;
 
-		if (uncle == nullptr || uncle->color == BLACK)
+		if (uncle == nullptr || uncle->color_ == BLACK)
 		{
-			if (cur->parent->parent->left == cur->parent && cur->parent->left == cur)
+			if (cur->parent_->parent_->left_ == cur->parent_ && cur->parent_->left_ == cur)
 			{
-				cur->parent->color = BLACK;
-				cur->parent->parent->color = RED;
-				turn_right(cur->parent->parent);
+				cur->parent_->color_ = BLACK;
+				cur->parent_->parent_->color_ = RED;
+				rotateRight_(cur->parent_->parent_);
 			}
-			else if (cur->parent->parent->right == cur->parent && cur->parent->right == cur)
+			else if (cur->parent_->parent_->right_ == cur->parent_ && cur->parent_->right_ == cur)
 			{
-				cur->parent->color = BLACK;
-				cur->parent->parent->color = RED;
-				turn_left(cur->parent->parent);
+				cur->parent_->color_ = BLACK;
+				cur->parent_->parent_->color_ = RED;
+				rotateLeft_(cur->parent_->parent_);
 			}
-			else if (cur->parent->parent->left == cur->parent && cur->parent->right == cur)
+			else if (cur->parent_->parent_->left_ == cur->parent_ && cur->parent_->right_ == cur)
 			{
-				cur->color = BLACK;
-				cur->parent->parent->color = RED;
-				turn_left(cur->parent);
-				turn_right(cur->parent);
+				cur->color_ = BLACK;
+				cur->parent_->parent_->color_ = RED;
+				rotateLeft_(cur->parent_);
+				rotateRight_(cur->parent_);
 			}
-			else if (cur->parent->parent->right == cur->parent && cur->parent->left == cur)
+			else if (cur->parent_->parent_->right_ == cur->parent_ && cur->parent_->left_ == cur)
 			{
-				cur->color = BLACK;
-				cur->parent->parent->color = RED;
-				turn_right(cur->parent);
-				turn_left(cur->parent);
+				cur->color_ = BLACK;
+				cur->parent_->parent_->color_ = RED;
+				rotateRight_(cur->parent_);
+				rotateLeft_(cur->parent_);
 			}
 		}
-		else if (uncle->color == RED)
+		else if (uncle->color_ == RED)
 		{
-			change_color(cur);
-			cur = cur->parent->parent;
-			root->color = BLACK;
+			changeColor_(cur);
+			cur = cur->parent_->parent_;
+			root_->color_ = BLACK;
 		}
 	}
 }
 
 template<typename T>
-void Tree<T>::deleteEl(const T& value)
+void Tree<T>::deleteElement(const T& key)
 {
-	Node<T> *cur = root, *parent = nullptr;
+	Node<T> * cur = search(key);
 	if (cur == nullptr)
 		return;
 
-	if (root->value == value)
+	if (cur->left_ == nullptr && cur->right_ == nullptr)
 	{
-		root = deleteRoot(root);
-		root->color = BLACK;
-		root->parent = nullptr;
+		count_--;
+		if (cur->parent_->left_ == cur)
+			cur->parent_->left_ = nullptr;
+		else //if (cur->parent_->right_ == cur)
+			cur->parent_->right_ = nullptr;
+	}
+	else if (cur->left_ != nullptr && cur->right_ == nullptr)
+	{
+		count_--;
+		if (cur->color_ == RED)
+		{
+			if (cur->parent_->left_ == cur)
+				cur->parent_->left_ = cur->left_;
+			else cur->parent_->right_ = cur->left_;
+		}
+		else
+		{
+			if (cur->left_->color_ == RED)
+			{
+				if (cur->parent_->left_ == cur)
+				{
+					cur->parent_->left_ = cur->left_;
+					cur->parent_->left_->color_ = BLACK;
+				}
+				else
+				{
+					cur->parent_->right_ = cur->left_;
+					cur->parent_->right_->color_ = BLACK;
+				}
+			}
+			else
+			{
+				if (cur->parent_->left_ == cur)
+				{
+					cur->parent_->left_ = cur->left_;
+					cur->parent_->left_->color_ = BLACK_BLACK;
+					deleteBalance_(cur->parent_->left_);
+				}
+				else
+				{
+					cur->parent_->right_ = cur->left_;
+					cur->parent_->right_->color_ = BLACK_BLACK;
+					deleteBalance_(cur->parent_->right_);
+				}
+			}
+		}
 	}
 	else
 	{
-		parent = root;
-		if (value < parent->value)
-			cur = parent->left;
-		else cur = parent->right;
-		while (cur)
-		{
-			if (cur->value == value)
-			{
-				if (value < parent->value)
-					parent->left = deleteRoot(parent->left);
-				else parent->right = deleteRoot(parent->right);
-				return;
-			}
-			parent = cur;
-			if (value < parent->value)
-				cur = parent->left;
-			else cur = parent->right;
-		}
+		Node<T> * min = cur->right_;
+		while (min->left_)
+			min = min->left_;
+		T a = min->key_;
+		deleteElement(min->key_);
+		cur->key_ = a;
 	}
 }
 template<typename T>
-Node<T> * Tree<T>::deleteRoot(Node<T> * head)
+void Tree<T>::deleteBalance_(Node<T> * node)
 {
-	Node<T>* cur = nullptr, *parent = nullptr;
-	if (head)
+	Node<T> * cur = node;
+
+	while (cur->color_ == BLACK_BLACK)
 	{
-		count--;
-		cur = head->right;
-		if (!cur)
+		Node<T> * brother = sibling_(cur);
+		if ((brother->left_ && brother->left_->color_ == RED) || (brother->right_ && brother->right_->color_ == RED))
 		{
-			cur = head->left;
-		}
-		else
-		{
-			if (cur->left)
+			if (cur->parent_->left_ == cur)
 			{
-				parent = head;
-				while (cur->left)
+				if (!brother->right_ || brother->right_->color_ == BLACK)
 				{
-					parent = cur;
-					cur = cur->left;
+					brother->left_->color_ = BLACK;
+					brother->color_ = RED;
+					rotateRight_(brother);
 				}
-				parent->left = cur->right;
-				cur->right = head->right;
-			}
-			cur->left = head->left;
-		}
-		deleteBalance(head);
-		delete head;
-		return cur;
-	}
-	return nullptr;
-}
-template<typename T>
-void Tree<T>::deleteBalance(Node<T> * node)
-{
-	Node<T>* sib = nullptr;
-	while (node && (node != root) && (node->color == BLACK))
-	{
-		sib = sibling(node);
-		if (sib && (sib == sib->parent->right))
-		{
-			if (sib->color == RED)
-			{
-				sib->color = BLACK;
-				node->parent->color = RED;
-				turn_left(node->parent);
-				sib = sibling(node);
-			}
-			if ((!sib->left || sib->left->color == BLACK) && (!sib->right || sib->right->color == BLACK))
-			{
-				sib->color = RED;
-				node = node->parent;
+				else
+				{
+					brother->right_->color_ = BLACK;
+					brother->color_ = cur->parent_->color_;
+					cur->parent_->color_ = BLACK;
+					cur->color_ = BLACK;
+					rotateLeft_(cur->parent_);
+				}
 			}
 			else
 			{
-				if (!sib->right || sib->right->color == BLACK)
+				if (brother->right_ && brother->left_->color_ == BLACK)
 				{
-					sib->left->color = BLACK;
-					sib->color = RED;
-					turn_right(sib);
-					sib = sibling(node);
+					brother->right_->color_ = BLACK;
+					brother->color_ = RED;
+					rotateLeft_(brother);
 				}
-				sib->color = node->parent->color;
-				node->parent->color = BLACK;
-				sib->right->color = BLACK;
-				turn_left(node->parent);
-				node = root;
+				else
+				{
+					brother->left_->color_ = BLACK;
+					brother->color_ = cur->parent_->color_;
+					cur->parent_->color_ = BLACK;
+					cur->color_ = BLACK;
+					rotateRight_(cur->parent_);
+				}
 			}
 		}
 		else
 		{
-			if (sib->color == RED)
+			if (cur->parent_->color_ == RED)
 			{
-				sib->color = BLACK;
-				node->parent->color = RED;
-				turn_right(node->parent);
-				sib = sibling(node);
-			}
-			if ((!sib->right || sib->right->color == BLACK) && (!sib->left || sib->left->color == BLACK))
-			{
-				sib->color = RED;
-				node = node->parent;
+				cur->parent_->color_ = BLACK;
+				brother->color_ = RED;
+				cur->color_ = BLACK;
 			}
 			else
 			{
-				if (!sib || sib->left->color == BLACK)
+				if (brother->color_ == BLACK)
 				{
-					sib->right->color = BLACK;
-					sib->color = RED;
-					turn_left(sib);
-					sib = sibling(node);
+					cur->parent_->color_ = BLACK_BLACK;
+					brother->color_ = RED;
+					cur->color_ = BLACK;
+					cur = cur->parent_;
 				}
-				sib->color = node->parent->color;
-				node->parent->color = BLACK;
-				sib->left->color = BLACK;
-				turn_right(node->parent);
-				node = root;
+				else if (brother->color_ == RED)
+				{
+					if (cur->parent_->left_ == cur)
+					{
+						brother->color_ = BLACK;
+						cur->parent_->color_ = RED;
+						rotateLeft_(cur->parent_);
+					}
+					else
+					{
+						brother->color_ = BLACK;
+						cur->parent_->color_ = RED;
+						rotateRight_(cur->parent_);
+					}
+				}
 			}
 		}
 	}
-	node->color = BLACK;
 }
 
 template<typename T>
-void Tree<T>::change_color(Node<T> * node)
+void Tree<T>::changeColor_(Node<T> * node)
 {
-	node->parent->parent->left->color = node->parent->parent->right->color = BLACK;
-	node->parent->parent->color = RED;
+	node->parent_->parent_->left_->color_ = node->parent_->parent_->right_->color_ = BLACK;
+	node->parent_->parent_->color_ = RED;
 }
 template<typename T>
-void Tree<T>::turn_right(Node<T> * node)
+void Tree<T>::rotateRight_(Node<T> * node)
 {
-	Node<T> * a = node->left;
-	node->left = a->right;
-	if (a->right)
-		a->right->parent = node;
-	a->parent = node->parent;
-	if (node->parent == nullptr)
-		root = a;
-	else if (node == node->parent->right)
-		node->parent->right = a;
-	else node->parent->left = a;
-	a->right = node;
-	node->parent = a;
+	Node<T> * a = node->left_;
+	node->left_ = a->right_;
+	if (a->right_)
+		a->right_->parent_ = node;
+	a->parent_ = node->parent_;
+	if (node->parent_ == nullptr)
+		root_ = a;
+	else if (node == node->parent_->right_)
+		node->parent_->right_ = a;
+	else node->parent_->left_ = a;
+	a->right_ = node;
+	node->parent_ = a;
 }
 template<typename T>
-void Tree<T>::turn_left(Node<T> * node)
+void Tree<T>::rotateLeft_(Node<T> * node)
 {
-	Node<T> * a = node->right;
-	node->right = a->left;
-	if (a->left)
-		a->left->parent = node;
-	a->parent = node->parent;
-	if (node->parent == nullptr)
-		root = a;
-	else if (node == node->parent->left)
-		node->parent->left = a;
-	else node->parent->right = a;
-	a->left = node;
-	node->parent = a;
+	Node<T> * a = node->right_;
+	node->right_ = a->left_;
+	if (a->left_)
+		a->left_->parent_ = node;
+	a->parent_ = node->parent_;
+	if (node->parent_ == nullptr)
+		root_ = a;
+	else if (node == node->parent_->left_)
+		node->parent_->left_ = a;
+	else node->parent_->right_ = a;
+	a->left_ = node;
+	node->parent_ = a;
 }
 template<typename T>
-Node<T> * Tree<T>::sibling(Node<T> * node)
+Node<T> * Tree<T>::sibling_(Node<T> * node)
 {
-	if (node && node->parent)
+	if (node && node->parent_)
 	{
-		if (node->parent->left == node)
-			return node->parent->right;
-		else return node->parent->left;
+		if (node->parent_->left_ == node)
+			return node->parent_->right_;
+		else return node->parent_->left_;
 	}
 	return nullptr;
 }
